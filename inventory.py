43f0a07450470f042e5c36d830d5b1b6ee8d38bb
thomas7 <thomas7@vt.edu>
2012-09-26 19:31:33 -0400

    parts

--- inventory.py
+++ inventory.py
@@ -62,10 +62,7 @@ def list_funct(arg):
                         writer.writerow(item)
             if arg.find("sort") > 0:
                 (beginning,  sort,  field)  = arg.partition(" sort by ")
-                if field == "Quantity":
-                    sortedList = sorted(dictList,  key =  itemgetter(field)
-                else:
-                    sortedList = sorted(dictList,  key = itemgetter(field)
+                sortedList = sorted(dictList,  key = itemgetter(field))
                 writer.writeheader()
                 writer.writerows(sortedList)
         except KeyError as e:
